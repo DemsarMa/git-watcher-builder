2f17196413b47578ddaa1cd3754a0fde6281c2bb
Matt <78916962+DemsarMa@users.noreply.github.com>
2022-07-12 12:01:41 +0200

    Fixed dumb errors

--- main.sh
+++ main.sh
@@ -5,8 +5,8 @@
 
 # Builds are headed to repos/builds
 # In case you want to add more repos, clone 
-# them in repos directory, copy lines from 34
-# to 35 and paste respectively after 50th line 
+# them in repos directory, copy lines from 32
+# to 33 and paste respectively after 48th line 
 # (Example: `screen -S <repo_name> -X stuff './git-repo-watcher -d ~/repos/<repo_name>" -h ~/repos/hooks/<repo_name>\n'`) 
 # Then create a new hook file for building, look 
 # https://github.com/kolbasa/git-repo-watcher for more info. 
@@ -29,24 +29,22 @@ screen -dmS title
 screen -dmS geyser
 screen -dmS protocol
 
-cd git-repo-watcher
-
-screen -S ecob -X stuff './git-repo-watcher -d ~/repos/EcoBosses" -h ~/repos/hooks/ecobosses\n'
+screen -S ecob -X stuff './git-repo-watcher -d repos/EcoBosses -h repos/hooks/ecobosses\n'
 echo "Started watching EcoBosses for changes!"
 
-screen -S ecoe -X stuff './git-repo-watcher -d ~/repos/EcoEnchants" -h ~/repos/hooks/ecoenchants\n'
+screen -S ecoe -X stuff './git-repo-watcher -d repos/EcoEnchants -h repos/hooks/ecoenchants\n'
 echo "Started watching EcoEnchants for changes!"
 
-screen -S plotq -X stuff './git-repo-watcher -d ~/repos/PlotSquared" -h ~/repos/hooks/plotsquared\n'
+screen -S plotq -X stuff './git-repo-watcher -d repos/PlotSquared -h repos/hooks/plotsquared\n'
 echo "Started watching PlotSquared for changes!"
 
-screen -S title -X stuff './git-repo-watcher -d ~/repos/TitleManager" -h ~/repos/hooks/titlemanager\n'
+screen -S title -X stuff './git-repo-watcher -d repos/TitleManager -h repos/hooks/titlemanager\n'
 echo "Started watching TitleManager for changes!"
 
-screen -S geyser -X stuff './git-repo-watcher -d ~/repos/Geyser" -h ~/repos/hooks/Geyser\n'
+screen -S geyser -X stuff './git-repo-watcher -d repos/Geyser -h repos/hooks/Geyser\n'
 echo "Started watching Geyser for changes!"
 
-screen -S protocol -X stuff './git-repo-watcher -d ~/repos/ProtocolLib" -h ~/repos/hooks/protocollib\n'
+screen -S protocol -X stuff './git-repo-watcher -d repos/ProtocolLib -h repos/hooks/protocollib\n'
 echo "Started watching ProtocolLib for changes!"
-session -list
+screen -list
 echo "Builds will be exported to $HOME/repos/builds upon new changes!"
